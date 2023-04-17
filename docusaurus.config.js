5461e560be7d45b682982152e3e35324cc83baba
itaquito <ichilaquiles@gmail.com>
2023-04-17 15:36:16 -0600

    Fixes links issue

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,118 +1,114 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
-/** @type {import('@docusaurus/types').Config} */
+/** @type {import("@docusaurus/types").Config} */
 const config = {
-  title: 'Python',
-  tagline: 'Nuestro reporte de Python',
-  favicon: 'favicon.ico',
+  title: "Python",
+  tagline: "Nuestro reporte de Python",
+  favicon: "favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://python.itaqui.to',
-  baseUrl: '/',
+  url: "https://python.itaqui.to",
+  baseUrl: "/",
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internalization, you can use this field to set useful
+  // Even if you don"t use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'es',
-    locales: ['es'],
+    defaultLocale: "es",
+    locales: ["es"],
   },
 
   presets: [
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
+      "classic",
+      /** @type {import("@docusaurus/preset-classic").Options} */
       ({
         docs: {
           routeBasePath: "/",
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           editUrl:
-            'https://github.com/itaquito/python/tree/main/',
+            "https://github.com/itaquito/python/tree/main/",
         },
         blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
   ],
 
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    /** @type {import("@docusaurus/preset-classic").ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: 'banner.png',
+      // Replace with your project"s social card
+      image: "banner.png",
       docs: {
         sidebar: {
           hideable: true
         }
       },
       navbar: {
-        title: 'Python',
+        title: "Python",
         logo: {
-          alt: 'Logo de Python',
-          src: 'logo.svg',
+          alt: "Logo de Python",
+          src: "logo.svg",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'mySidebar',
-            position: 'left',
-            label: 'Empezar',
+            type: "docSidebar",
+            sidebarId: "mySidebar",
+            position: "left",
+            label: "Empezar",
           },
           {
-            href: 'https://github.com/itaquito/python',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/itaquito/python",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: "Empezar",
+                to: "/intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                label: "Twitter",
+                href: "https://twitter.com/docusaurus",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: "Contribuye en GitHub",
+                href: "https://github.com/itaquito/python",
               },
             ],
           },
