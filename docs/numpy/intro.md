08a924ece1eb8667abe7a073cb5f9f3f088f40d1
itaquito <ichilaquiles@gmail.com>
2023-08-30 09:37:38 -0600

    feat: Adds NumPy category

---
+++ docs/numpy/intro.md
@@ -0,0 +1,22 @@
+---
+sidebar_label: '🌟 Introducción'
+sidebar_position: 1
+---
+
+# 🌟 Introducción
+
+NumPy es posiblemente la librería más popular en Python. Le añade soporte a este lenguaje para poder trabajar con arreglos multidimensionales y matrices, agregando métodos para poder crearlos, modificarlos, leerlos y manipularlos. De igual manera agrega una gran cantidad de herramientas matemáticas de alto nivel.
+
+## 🎠 ¿Cómo instalar NumPy?
+
+El único requisito de NumPy es Python por si mismo. Podemos instalar NumPy con ayuda de `pip`, ejecutando este comando en nuestra linea de comandos o en nuestro entorno virtual:
+
+```bash title="Instalar NumPy con pip"
+pip install numpy
+```
+
+O si estamos usando Conda, podemos utilizar el siguiente comando:
+
+```bash title="Instalar NumPy con Conda"
+conda install numpy
+```
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,44 +1,43 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import("@docusaurus/types").Config} */
 const config = {
-  title: "Python",
-  tagline: "Nuestro reporte de Python",
-  favicon: "favicon.ico",
+  title: 'Python',
+  tagline: 'Nuestro reporte de Python',
+  favicon: 'favicon.ico',
 
   // Set the production url of your site here
-  url: "https://python.itaqui.to",
-  baseUrl: "/",
+  url: 'https://python.itaqui.to',
+  baseUrl: '/',
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don"t use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "es",
-    locales: ["es"],
+    defaultLocale: 'es',
+    locales: ['es'],
   },
 
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import("@docusaurus/preset-classic").Options} */
       ({
         docs: {
-          routeBasePath: "/",
-          sidebarPath: require.resolve("./sidebars.js"),
-          editUrl:
-            "https://github.com/itaquito/python/tree/main/",
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/itaquito/python/tree/main/',
         },
         blog: false,
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
@@ -48,103 +47,112 @@ const config = {
     /** @type {import("@docusaurus/preset-classic").ThemeConfig} */
     ({
       // Replace with your project"s social card
-      image: "banner.png",
+      image: 'banner.png',
       docs: {
         sidebar: {
-          hideable: true
-        }
+          hideable: true,
+        },
       },
       navbar: {
-        title: "Python",
+        title: 'Python',
         logo: {
-          alt: "Logo de Python",
-          src: "logo.svg",
+          alt: 'Logo de Python',
+          src: 'logo.svg',
         },
         items: [
           {
-            type: "docSidebar",
-            sidebarId: "mySidebar",
-            position: "left",
-            label: "Introducción",
+            type: 'docSidebar',
+            sidebarId: 'mySidebar',
+            position: 'left',
+            label: 'Introducción',
           },
           {
-            label: "Conceptos básicos",
-            position: "left",
-            to: "/category/conceptos-basicos",
+            label: 'Conceptos básicos',
+            position: 'left',
+            to: '/category/conceptos-basicos',
           },
           {
-            label: "Conceptos avanzados",
-            position: "left",
-            to: "/category/conceptos-avanzados",
+            label: 'Conceptos avanzados',
+            position: 'left',
+            to: '/category/conceptos-avanzados',
           },
           {
-            label: "Programación orientada a objetos",
-            position: "left",
-            to: "/category/programacion-orientada-a-objetos",
+            label: 'Programación orientada a objetos',
+            position: 'left',
+            to: '/category/programacion-orientada-a-objetos',
           },
           {
-            href: "https://github.com/itaquito/python",
-            label: "GitHub",
-            position: "right",
+            label: 'NumPy',
+            position: 'left',
+            to: '/category/numpy',
+          },
+          {
+            href: 'https://github.com/itaquito/python',
+            label: 'GitHub',
+            position: 'right',
           },
         ],
       },
       footer: {
-        style: "dark",
+        style: 'dark',
         links: [
           {
-            title: "Secciones",
+            title: 'Secciones',
             items: [
               {
-                label: "Introducción",
-                to: "/intro",
+                label: 'Introducción',
+                to: '/intro',
               },
               {
-                label: "Conceptos básicos",
-                to: "/category/conceptos-basicos",
+                label: 'Conceptos básicos',
+                to: '/category/conceptos-basicos',
               },
               {
-                label: "Conceptos avanzados",
-                to: "/category/conceptos-avanzados",
+                label: 'Conceptos avanzados',
+                to: '/category/conceptos-avanzados',
               },
               {
-                label: "Programación orientada a objetos",
-                to: "/category/programacion-orientada-a-objetos",
+                label: 'Programación orientada a objetos',
+                to: '/category/programacion-orientada-a-objetos',
               },
             ],
           },
           {
-            title: "Otras páginas interesantes",
+            title: 'Otras páginas interesantes',
             items: [
               {
-                label: "Página oficial de Python",
-                href: "https://www.python.org/"
+                label: 'Página oficial de Python',
+                href: 'https://www.python.org/',
               },
               {
-                label: "El Libro De Python",
-                href: "https://ellibrodepython.com/"
+                label: 'El Libro De Python',
+                href: 'https://ellibrodepython.com/',
               },
               {
-                label: "Contribuyentes de esta página",
-                href: "https://github.com/itaquito/python/graphs/contributors"
+                label: 'Contribuyentes de esta página',
+                href: 'https://github.com/itaquito/python/graphs/contributors',
               },
               {
-                label: "Código fuente de Python",
-                href: "https://github.com/python/cpython"
-              }
+                label: 'Código fuente de Python',
+                href: 'https://github.com/python/cpython',
+              },
+              {
+                label: 'Página oficial de NumPy',
+                href: 'https://numpy.org/',
+              },
             ],
           },
           {
-            title: "Descargas",
+            title: 'Descargas',
             items: [
               {
-                label: "Descarga Python",
-                href: "https://www.python.org/downloads/"
+                label: 'Descarga Python',
+                href: 'https://www.python.org/downloads/',
               },
               {
-                label: "Descarga Anaconda",
-                href: "https://www.anaconda.com/products/distribution"
-              }
+                label: 'Descarga Anaconda',
+                href: 'https://www.anaconda.com/products/distribution',
+              },
             ],
           },
         ],
